44992dafb195203505797de4c550017823760bd8
JetStormhl <72029945+JetStormhl@users.noreply.github.com>
2025-08-07 13:56:28 -0500

    25999

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.views.defaults import page_not_found
+
+handler404 = page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
